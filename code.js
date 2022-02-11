2133e1330ca28027375b91c94c29fa449f908035
LeonarthCG <angel.cantos.giron@gmail.com>
2022-02-11 23:21:13 +0100

    added filtering

--- code.js
+++ code.js
@@ -13,8 +13,7 @@ const noMon = {
         };
 
 //parse GET parameters
-var get = {}
-location.search.substr(1).split("&").forEach(function(item) {get[item.split("=")[0]] = item.split("=")[1]})
+const get = (new URL(window.location.href)).searchParams
 
 //initialize the json data
 var data;
@@ -30,23 +29,146 @@ xobj.send(null);
 const mons = data["mons"];
 
 //build the lists for each stage
+const defaultLevels = [
+	["Baby I"],
+	["Baby II"],
+	["Child"],
+	["Adult"],
+	["Perfect"],
+	["Ultimate"]
+]
+
+const validLevels = [
+	["Baby I", "Armor", "Hybrid", "Unknown"],
+	["Baby II", "Armor", "Hybrid", "Unknown"],
+	["Child", "Armor", "Hybrid", "Unknown"],
+	["Adult", "Armor", "Hybrid", "Unknown"],
+	["Perfect", "Armor", "Hybrid", "Unknown"],
+	["Ultimate", "Armor", "Hybrid", "Unknown"]
+]
+
+const validAttributes = ["Vaccine", "Data", "Virus", "Free/None", "Variable", "Unknown"]
+
+const validFields = [
+	"Deep Savers",
+	"Nature Spirits",
+	"Virus Busters",
+	"Wind Guardians",
+	"Dragon's Roar",
+	"Nightmare Soldiers",
+	"Metal Empire",
+	"Jungle Troopers",
+	"Dark Area",
+	"Unknown",
+	"N/A"
+]
+
+const validAntibodies = ["Not a carrier", "Carrier", "Natural Carrier"]
+
+var settings = {
+	"level": [
+		get.getAll("l0Level[]"),
+		get.getAll("l1Level[]"),
+		get.getAll("l2Level[]"),
+		get.getAll("l3Level[]"),
+		get.getAll("l4Level[]"),
+		get.getAll("l5Level[]")
+	],
+	"attribute": [
+		get.getAll("l0Attribute[]"),
+		get.getAll("l1Attribute[]"),
+		get.getAll("l2Attribute[]"),
+		get.getAll("l3Attribute[]"),
+		get.getAll("l4Attribute[]"),
+		get.getAll("l5Attribute[]")
+	],
+	"field": [
+		get.getAll("l0Field[]"),
+		get.getAll("l1Field[]"),
+		get.getAll("l2Field[]"),
+		get.getAll("l3Field[]"),
+		get.getAll("l4Field[]"),
+		get.getAll("l5Field[]")
+	],
+	"antibody": [
+		get.getAll("l0Antibody[]"),
+		get.getAll("l1Antibody[]"),
+		get.getAll("l2Antibody[]"),
+		get.getAll("l3Antibody[]"),
+		get.getAll("l4Antibody[]"),
+		get.getAll("l5Antibody[]")
+	]
+}
+
+for (var i = 0; i <= 5; i++) {
+	if (settings["level"][i] != undefined) {
+		for (const level of settings["level"][i]) {
+			if (!validLevels[i].includes(level)) {
+				const index = settings["level"][i].indexOf(level);
+				if (index !== -1) {
+					settings["level"][i].splice(index, 1);
+				}
+			}
+		}
+	}
+	if (settings["attribute"][i] != undefined) {
+		for (const attribute of settings["attribute"][i]) {
+			if (!validAttributes.includes(attribute)) {
+				const index = settings["attribute"][i].indexOf(attribute);
+				if (index !== -1) {
+					settings["attribute"][i].splice(index, 1);
+				}
+			}
+		}
+	}
+	if (settings["field"][i] != undefined) {
+		for (const field of settings["field"][i]) {
+			if (!validFields.includes(field)) {
+				const index = settings["field"][i].indexOf(field);
+				if (index !== -1) {
+					settings["field"][i].splice(index, 1);
+				}
+			}
+		}
+	}
+	if (settings["antibody"][i] != undefined) {
+		for (const antibody of settings["antibody"][i]) {
+			if (!validAntibodies.includes(antibody)) {
+				const index = settings["antibody"][i].indexOf(antibody);
+				if (index !== -1) {
+					settings["antibody"][i].splice(index, 1);
+				}
+			}
+		}
+	}
+	if (settings["level"][i] == undefined || settings["level"][i].length == 0) {
+		settings["level"][i] = defaultLevels[i]
+	}
+	if (settings["attribute"][i] == undefined || settings["attribute"][i].length == 0) {
+		settings["attribute"][i] = validAttributes
+	}
+	if (settings["field"][i] == undefined || settings["field"][i].length == 0) {
+		settings["field"][i] = validFields
+	}
+	if (settings["antibody"][i] == undefined || settings["antibody"][i].length == 0) {
+		settings["antibody"][i] = validAntibodies
+	}
+}
 
 //check that the current mons are valid
 var currentMons = [
-		mons.find(item => item.title === decodeURIComponent(get["m0"])),
-		mons.find(item => item.title === decodeURIComponent(get["m1"])),
-		mons.find(item => item.title === decodeURIComponent(get["m2"])),
-		mons.find(item => item.title === decodeURIComponent(get["m3"])),
-		mons.find(item => item.title === decodeURIComponent(get["m4"])),
-		mons.find(item => item.title === decodeURIComponent(get["m5"]))
+		mons.find(item => item.title === get.get("m0")),
+		mons.find(item => item.title === get.get("m1")),
+		mons.find(item => item.title === get.get("m2")),
+		mons.find(item => item.title === get.get("m3")),
+		mons.find(item => item.title === get.get("m4")),
+		mons.find(item => item.title === get.get("m5"))
 	];
+
 for (var i = 0; i < currentMons.length; i++) {
 	if (currentMons[i] == undefined) {
 		currentMons[i] = noMon;
 	}
-}
-for (var i = 0; i < currentMons.length; i++) {
-	//check if mon is in the list for its level, if not then set to noMon
 }
 
 //update the url
@@ -60,14 +182,37 @@ function init() {
 }
 
 function listOptions(n) {
-	//<option disabled selected value/>
 	var list = [];
-	var content = "";
 	for (const mon of mons) {
-		if (mon["levels"].includes(levels[n])) {
+		var validLevel = false
+		for (const level of settings["level"][n]) {
+			if (mon["levels"].includes(level)) {
+				validLevel = true;
+			}
+		}
+		var validAttribute = false
+		for (const attribute of settings["attribute"][n]) {
+			if (mon["attributes"].includes(attribute)) {
+				validAttribute = true;
+			}
+		}
+		var validField = false
+		for (const field of settings["field"][n]) {
+			if (mon["fields"].includes(field)) {
+				validField = true;
+			}
+		}
+		var validAntibody = false
+		for (const antibody of settings["antibody"][n]) {
+			if (mon["xAntibody"].includes(antibody)) {
+				validAntibody = true;
+			}
+		}
+		if (validLevel && validAttribute && validField && validAntibody) {
 			list.push(mon["title"]);
 		}
 	}
+	var content = "<option hidden disabled selected value> - " + list.length + " Results - Select a mon -</option>"; //make the list start with no selection
 	for (const title of list) {
 		content += "<option value=\"" + title + "\">" + title + "</option>";
 	}
@@ -76,6 +221,7 @@ function listOptions(n) {
 
 function updateUrl() {
 	var newUrl = "?";
+	
 	var i = 0;
 	for (const mon of currentMons) {
 		if (mon !== noMon) {
@@ -83,6 +229,22 @@ function updateUrl() {
 		}
 		i++;
 	}
+	
+	for (i = 0; i <= 5; i++) {
+		for (const level of settings["level"][i]) {
+			newUrl += "l" + i + "Level[]=" + level + "&";
+		}
+		for (const attribute of settings["attribute"][i]) {
+			newUrl += "l" + i + "Attribute[]=" + attribute + "&";
+		}
+		for (const field of settings["field"][i]) {
+			newUrl += "l" + i + "Field[]=" + field + "&";
+		}
+		for (const antibody of settings["antibody"][i]) {
+			newUrl += "l" + i + "Antibody[]=" + antibody + "&";
+		}
+	}
+	
 	window.history.pushState("", "Title", newUrl);
 }
 
@@ -106,4 +268,39 @@ function onSelect(title, n) {
 	currentMons[n] = mons.find(item => item.title === title);
 	paintMon(n);
 	updateUrl();
+}
+
+function updateCheckboxes() {
+	for (var i = 0; i <= 5; i++) {
+		for (level of settings["level"][i]) {
+			document.getElementById("c" + i + "Level" + level).checked = true;
+		}
+		for (attribute of settings["attribute"][i]) {
+			document.getElementById("c" + i + "Attribute" + attribute).checked = true;
+		}
+		for (field of settings["field"][i]) {
+			document.getElementById("c" + i + "Field" + field).checked = true;
+		}
+		for (antibody of settings["antibody"][i]) {
+			document.getElementById("c" + i + "Antibody" + antibody).checked = true;
+		}
+	}
+}
+
+function updateSetting(e, n) {
+	const index = settings[e.name][n].indexOf(e.value);
+	if (e.checked) {
+		if (index == -1) {
+			settings[e.name][n].push(e.value);
+		}
+	} else {
+		if (index !== -1) {
+			settings[e.name][n].splice(index, 1);
+		}
+	}
+	updateUrl();
+}
+
+function refresh() {
+	location.reload();
 }
