5e26323b6c16b93f1f0dd20897e51250796d2762
LeonarthCG <angel.cantos.giron@gmail.com>
2022-02-15 21:16:01 +0100

    made GET parameters not massive
    the different parameters and arrays have been consolidated into a single parameter for each stage
    
    the parameters take a radix 32 string, with a bit for each checkbox

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
-//constants
-const levels = ["Baby I", "Baby II", "Child", "Adult", "Perfect", "Ultimate"];
+//empty mon, for when an invalid mon is passed
 const noMon = {
             "title": "N/A",
             "dubs": ["N/A"],
@@ -11,6 +10,39 @@ const noMon = {
             "fields": ["N/A"],
             "xAntibody": "N/A"
         };
+
+//default and valid possibilities for the different settings
+const defaultLevels = [
+	["Baby I"],
+	["Baby II"],
+	["Child"],
+	["Adult"],
+	["Perfect"],
+	["Ultimate"]
+]
+const validLevels = [
+	["Baby I", "Armor", "Hybrid", "Unknown"],
+	["Baby II", "Armor", "Hybrid", "Unknown"],
+	["Child", "Armor", "Hybrid", "Unknown"],
+	["Adult", "Armor", "Hybrid", "Unknown"],
+	["Perfect", "Armor", "Hybrid", "Unknown"],
+	["Ultimate", "Armor", "Hybrid", "Unknown"]
+]
+const validAttributes = ["Vaccine", "Data", "Virus", "Free/None", "Variable", "Unknown"]
+const validFields = [
+	"Deep Savers",
+	"Nature Spirits",
+	"Virus Busters",
+	"Wind Guardians",
+	"Dragon's Roar",
+	"Nightmare Soldiers",
+	"Metal Empire",
+	"Jungle Troopers",
+	"Dark Area",
+	"Unknown",
+	"N/A"
+]
+const validAntibodies = ["Not a carrier", "Carrier", "Natural Carrier"]
 
 //parse GET parameters
 const get = (new URL(window.location.href)).searchParams
@@ -28,134 +60,61 @@ xobj.onreadystatechange = function () {
 xobj.send(null);
 const mons = data["mons"];
 
-//build the lists for each stage
-const defaultLevels = [
-	["Baby I"],
-	["Baby II"],
-	["Child"],
-	["Adult"],
-	["Perfect"],
-	["Ultimate"]
-]
-
-const validLevels = [
-	["Baby I", "Armor", "Hybrid", "Unknown"],
-	["Baby II", "Armor", "Hybrid", "Unknown"],
-	["Child", "Armor", "Hybrid", "Unknown"],
-	["Adult", "Armor", "Hybrid", "Unknown"],
-	["Perfect", "Armor", "Hybrid", "Unknown"],
-	["Ultimate", "Armor", "Hybrid", "Unknown"]
-]
-
-const validAttributes = ["Vaccine", "Data", "Virus", "Free/None", "Variable", "Unknown"]
-
-const validFields = [
-	"Deep Savers",
-	"Nature Spirits",
-	"Virus Busters",
-	"Wind Guardians",
-	"Dragon's Roar",
-	"Nightmare Soldiers",
-	"Metal Empire",
-	"Jungle Troopers",
-	"Dark Area",
-	"Unknown",
-	"N/A"
-]
-
-const validAntibodies = ["Not a carrier", "Carrier", "Natural Carrier"]
-
+//recover all of the settings, set to default if not found
 var settings = {
-	"level": [
-		get.getAll("l0Level[]"),
-		get.getAll("l1Level[]"),
-		get.getAll("l2Level[]"),
-		get.getAll("l3Level[]"),
-		get.getAll("l4Level[]"),
-		get.getAll("l5Level[]")
-	],
-	"attribute": [
-		get.getAll("l0Attribute[]"),
-		get.getAll("l1Attribute[]"),
-		get.getAll("l2Attribute[]"),
-		get.getAll("l3Attribute[]"),
-		get.getAll("l4Attribute[]"),
-		get.getAll("l5Attribute[]")
-	],
-	"field": [
-		get.getAll("l0Field[]"),
-		get.getAll("l1Field[]"),
-		get.getAll("l2Field[]"),
-		get.getAll("l3Field[]"),
-		get.getAll("l4Field[]"),
-		get.getAll("l5Field[]")
-	],
-	"antibody": [
-		get.getAll("l0Antibody[]"),
-		get.getAll("l1Antibody[]"),
-		get.getAll("l2Antibody[]"),
-		get.getAll("l3Antibody[]"),
-		get.getAll("l4Antibody[]"),
-		get.getAll("l5Antibody[]")
-	]
+	"level": [[], [], [], [], [], []],
+	"attribute": [[], [], [], [], [], []],
+	"field": [[], [], [], [], [], []],
+	"antibody": [[], [], [], [], [], []]
 }
 
 for (var i = 0; i <= 5; i++) {
-	if (settings["level"][i] != undefined) {
-		for (const level of settings["level"][i]) {
-			if (!validLevels[i].includes(level)) {
-				const index = settings["level"][i].indexOf(level);
-				if (index !== -1) {
-					settings["level"][i].splice(index, 1);
-				}
-			}
+	//recover the settings radix 32 string, if it exists
+	const n = get.get("s" + i);
+	//convert it to integer
+	const s = (n == undefined || n == null) ? 0 : parseInt(n, 32);
+	//for each setting
+	for (var j = 0; j < 4; j++) {
+		//if the corresponding bit is set
+		if (s & (1 << j)) {
+			//add it to the list
+			settings["level"][i].push(validLevels[i][j]);
 		}
 	}
-	if (settings["attribute"][i] != undefined) {
-		for (const attribute of settings["attribute"][i]) {
-			if (!validAttributes.includes(attribute)) {
-				const index = settings["attribute"][i].indexOf(attribute);
-				if (index !== -1) {
-					settings["attribute"][i].splice(index, 1);
-				}
-			}
+	//same as above
+	for (var j = 4; j < 10; j++) {
+		if (s & (1 << j)) {
+			settings["attribute"][i].push(validAttributes[j - 4]);
 		}
 	}
-	if (settings["field"][i] != undefined) {
-		for (const field of settings["field"][i]) {
-			if (!validFields.includes(field)) {
-				const index = settings["field"][i].indexOf(field);
-				if (index !== -1) {
-					settings["field"][i].splice(index, 1);
-				}
-			}
+	for (var j = 10; j < 21; j++) {
+		if (s & (1 << j)) {
+			settings["field"][i].push(validFields[j - 10]);
 		}
 	}
-	if (settings["antibody"][i] != undefined) {
-		for (const antibody of settings["antibody"][i]) {
-			if (!validAntibodies.includes(antibody)) {
-				const index = settings["antibody"][i].indexOf(antibody);
-				if (index !== -1) {
-					settings["antibody"][i].splice(index, 1);
-				}
-			}
+	for (var j = 21; j < 24; j++) {
+		if (s & (1 << j)) {
+			settings["antibody"][i].push(validAntibodies[j - 21]);
 		}
 	}
-	if (settings["level"][i] == undefined || settings["level"][i].length == 0) {
-		settings["level"][i] = defaultLevels[i]
+	//if the settings are empty, set to default
+	if (settings["level"][i].length == 0) {
+		settings["level"][i] = defaultLevels[i];
 	}
-	if (settings["attribute"][i] == undefined || settings["attribute"][i].length == 0) {
-		settings["attribute"][i] = validAttributes
+	//same as above
+	if (settings["attribute"][i].length == 0) {
+		settings["attribute"][i] = validAttributes;
 	}
-	if (settings["field"][i] == undefined || settings["field"][i].length == 0) {
-		settings["field"][i] = validFields
+	if (settings["field"][i].length == 0) {
+		settings["field"][i] = validFields;
 	}
-	if (settings["antibody"][i] == undefined || settings["antibody"][i].length == 0) {
-		settings["antibody"][i] = validAntibodies
+	if (settings["antibody"][i].length == 0) {
+		settings["antibody"][i] = validAntibodies;
 	}
+	
 }
 
-//check that the current mons are valid
+//get the mons from the GET parameters
 var currentMons = [
 		mons.find(item => item.title === get.get("m0")),
 		mons.find(item => item.title === get.get("m1")),
@@ -165,8 +124,11 @@ var currentMons = [
 		mons.find(item => item.title === get.get("m5"))
 	];
 
+//TODO: maybe check that the current mons are valid (are in the list)?
+
+//replace the mons that were not found, with the empty mon
 for (var i = 0; i < currentMons.length; i++) {
-	if (currentMons[i] == undefined) {
+	if (currentMons[i] == undefined || currentMons[i] == null) {
 		currentMons[i] = noMon;
 	}
 }
@@ -175,21 +137,28 @@ for (var i = 0; i < currentMons.length; i++) {
 updateUrl();
 
 function init() {
+	//paint every mon
 	for (var i = 0; i < currentMons.length; i++) {
 		paintMon(i);
 		listOptions(i);
 	}
 }
 
+//update the selection list to include only the matching mons
 function listOptions(n) {
 	var list = [];
+	//for each mon in our json
 	for (const mon of mons) {
 		var validLevel = false
+		//for each value in the settings
 		for (const level of settings["level"][n]) {
+			//check if the mon has this attribute
 			if (mon["levels"].includes(level)) {
+				//if there's even one match, the attribute is valid
 				validLevel = true;
 			}
 		}
+		//same as above
 		var validAttribute = false
 		for (const attribute of settings["attribute"][n]) {
 			if (mon["attributes"].includes(attribute)) {
@@ -208,10 +177,12 @@ function listOptions(n) {
 				validAntibody = true;
 			}
 		}
+		//if all the checks have been passed, add it to the list
 		if (validLevel && validAttribute && validField && validAntibody) {
 			list.push(mon["title"]);
 		}
 	}
+	//finally, write the list
 	var content = "<option hidden disabled selected value> - " + list.length + " Results - Select a mon -</option>"; //make the list start with no selection
 	for (const title of list) {
 		content += "<option value=\"" + title + "\">" + title + "</option>";
@@ -221,7 +192,7 @@ function listOptions(n) {
 
 function updateUrl() {
 	var newUrl = "?";
-	
+	//for each mon, add their name
 	var i = 0;
 	for (const mon of currentMons) {
 		if (mon !== noMon) {
@@ -229,32 +200,54 @@ function updateUrl() {
 		}
 		i++;
 	}
-	
+	//for each mon, add their settings
 	for (i = 0; i <= 5; i++) {
-		for (const level of settings["level"][i]) {
-			newUrl += "l" + i + "Level[]=" + level + "&";
+		newUrl += "s" + i + "="
+		var s = 0;
+		//for each possible settings
+		for (var j = 0; j < 4; j++) {
+			const level = validLevels[i][j];
+			//if that settings is set
+			if (settings["level"][i].includes(level)) {
+				s |= 1 << j; //set the corresponding bit of our settings number
+			}
 		}
-		for (const attribute of settings["attribute"][i]) {
-			newUrl += "l" + i + "Attribute[]=" + attribute + "&";
+		//same as above
+		for (var j = 4; j < 10; j++) { 
+			const attribute = validAttributes[j - 4];
+			if (settings["attribute"][i].includes(attribute)) {
+				s |= 1 << j;
+			}
 		}
-		for (const field of settings["field"][i]) {
-			newUrl += "l" + i + "Field[]=" + field + "&";
+		for (var j = 10; j < 21; j++) { 
+			const field = validFields[j - 10];
+			if (settings["field"][i].includes(field)) {
+				s |= 1 << j;
+			}
 		}
-		for (const antibody of settings["antibody"][i]) {
-			newUrl += "l" + i + "Antibody[]=" + antibody + "&";
+		for (var j = 21; j < 24; j++) { 
+			const antibody = validAntibodies[j - 21];
+			if (settings["antibody"][i].includes(antibody)) {
+				s |= 1 << j;
+			}
 		}
+		//finally, add the resulting number to the url, in radix 32
+		newUrl += s.toString(32) + "&";
 	}
-	
+	//and update the url
 	window.history.pushState("", "Title", newUrl);
 }
 
 function paintMon(n) {
+	//get the mon to paint
 	var mon = currentMons[n];
+	//if there's no mon, don't try to draw its image
 	if (mon.url == "N/A") {
 		document.getElementById("m" + n + "Img").innerHTML = "";
 	} else {
 		document.getElementById("m" + n + "Img").innerHTML = "<a target=\"_blank\" href=\"" + mon.url + "\"><img src=\"" + mon.img + "\" alt=\"" + mon.title + "\" title=\"" + mon.title + "\"/></a>";
 	}
+	//the rest of the attributes we can just draw
 	document.getElementById("m" + n + "Name").innerHTML = mon.title;
 	document.getElementById("m" + n + "Dub").innerHTML = mon.dubs.join(', ');
 	document.getElementById("m" + n + "Level").innerHTML = mon.levels.join(', ');
@@ -265,13 +258,17 @@ function paintMon(n) {
 }
 
 function onSelect(title, n) {
+	//get the selected mon
 	currentMons[n] = mons.find(item => item.title === title);
+	//and update their page
 	paintMon(n);
 	updateUrl();
 }
 
 function updateCheckboxes() {
+	//for each mon
 	for (var i = 0; i <= 5; i++) {
+		//check if each setting is set, and update the checkbox accordingly
 		for (level of settings["level"][i]) {
 			document.getElementById("c" + i + "Level" + level).checked = true;
 		}
@@ -288,19 +285,23 @@ function updateCheckboxes() {
 }
 
 function updateSetting(e, n) {
+	//get the index of the setting, if it existed
 	const index = settings[e.name][n].indexOf(e.value);
+	//if the checkbox is checked
 	if (e.checked) {
+		//and the setting doesn't exist
 		if (index == -1) {
+			//add it
 			settings[e.name][n].push(e.value);
 		}
-	} else {
+	}
+	//if the checkbox is unchecked
+	else {
+		//and the setting exists
 		if (index !== -1) {
+			//remove it
 			settings[e.name][n].splice(index, 1);
 		}
 	}
 	updateUrl();
-}
-
-function refresh() {
-	location.reload();
 }
